c19b1d57622443ea565ff3eb29cb0372896a974b
Alan Alvarez <115573532+AlvarezNA@users.noreply.github.com>
2023-05-25 13:11:48 -0300

    finalizado
    Trabajo finalizado

--- TPO-23018-main/js/script.js
+++ TPO-23018-main/js/script.js
@@ -1,44 +1,52 @@
+/*function cerrarNav() {
+    var nav = document.getElementById("miNav");
 
-// get products data
+    if (nav.style.display === "none") {
+      nav.style.display = "block";
+    } else {
+      nav.style.display = "none";
+    }
+  }
+*/
+document.getElementById('formulario').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita el envío del formulario por defecto
 
-fetch("https://fakestoreapi.com/products/")
-  .then((response) => response.json())
-  .then((data) => {
-    const products = data.filter(
-      (product) => product.category === "electronics"
-    );
+  var nombre = document.getElementById('nombreF').value;
+  var apellido = document.getElementById('apellido').value;
+  var correo = document.getElementById('correo').value;
+  var telefono = document.getElementById('telefono').value;
+  var consulta = document.getElementById('consulta').value;
 
-    // Obtener el contenedor de los productos
-    const productContainer = document.getElementById("product-container");
+  // Verificar si los campos obligatorios están completos
+  if (nombre && apellido && correo && telefono && consulta) {
+    // Validar el nombre (más de 3 letras y solo letras)
+    if (nombre.length <= 3 || !/^[a-zA-Z]+$/.test(nombre)) {
+      alert('El nombre debe contener más de 5 letras y solo letras.');
+      return;
+    }
 
-    // Construir el HTML para cada producto
-    products.forEach((product) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
+    // Validar el apellido (más de 4 letras y solo letras)
+    if (apellido.length <= 4 || !/^[a-zA-Z]+$/.test(apellido)) {
+      alert('El apellido debe contener más de 5 letras y solo letras.');
+      return;
+    }
 
-      const image = document.createElement("img");
-      image.classList.add("img-card");
-      image.src = product.image;
-      image.alt = product.title;
+    // Validar la consulta (más de 20 letras)
+    if (consulta.length <= 20) {
+      alert('La consulta debe contener más de 20 letras.');
+      return;
+    }
 
-      const description = document.createElement("p");
-      description.classList.add("card-prod");
-      description.textContent = product.title;
+    // Validar el número de teléfono (exactamente 10 dígitos y solo números)
+    if (!/^\d{10}$/.test(telefono)) {
+      alert('El número de teléfono debe contener exactamente 10 números.');
+      return;
+    }
 
-      const button = document.createElement("button");
-      button.textContent = "Consultar";
-      button.addEventListener("click", () => {
-        // Aquí puedes definir la acción que deseas realizar al hacer clic en el botón
-        location.href = "contacto.html";
-      });
-
-      card.appendChild(image);
-      card.appendChild(description);
-      card.appendChild(button);
-
-      productContainer.appendChild(card);
-    });
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+    // Aquí puedes agregar el código para enviar el formulario
+    alert('¡Formulario enviado correctamente!');
+    document.getElementById('formulario').reset(); // Restablece el formulario después de enviarlo
+  } else {
+    alert('Por favor, complete todos los campos obligatorios.');
+  }
+});
